b12c77fb4a7bc1eb3b515ef85e001ad13cbd3a72
BLOOD-MD <nunu001936@gmail.com>
2026-08-01 02:17:40 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ const generateBtn = document.getElementById("generateBtn");
 const downloadBtn = document.getElementById("downloadBtn");
 const qrBox = document.getElementById("qrBox");
 
-let qrImage = null;
+let qrCanvas = null;
 
 
-generateBtn.addEventListener("click", () => {
+// Generate QR Code
+
+generateBtn.addEventListener("click", function () {
 
     const text = input.value.trim();
 
-    if(text === ""){
+
+    if (text === "") {
         alert("Please enter text or URL");
         return;
     }
@@ -19,24 +22,33 @@ generateBtn.addEventListener("click", () => {
     qrBox.innerHTML = "";
 
 
-    const img = document.createElement("img");
+    const canvas = document.createElement("canvas");
 
-    img.width = 250;
-    img.height = 250;
-
-    img.alt = "Generated QR Code";
+    qrBox.appendChild(canvas);
 
 
-    img.src =
-    "https://api.qrserver.com/v1/create-qr-code/?size=500x500&data="
-    + encodeURIComponent(text);
+    QRCode.toCanvas(
+        canvas,
+        text,
+        {
+            width: 300,
+            margin: 2
+        },
+        function (error) {
+
+            if (error) {
+
+                console.error(error);
+                alert("QR generation failed");
+
+                return;
+            }
+
+        }
+    );
 
 
-
-    qrBox.appendChild(img);
-
-
-    qrImage = img;
+    qrCanvas = canvas;
 
 
     downloadBtn.style.display = "block";
@@ -46,10 +58,12 @@ generateBtn.addEventListener("click", () => {
 
 
 
-downloadBtn.addEventListener("click", () => {
+// Download QR
+
+downloadBtn.addEventListener("click", function () {
 
 
-    if(!qrImage){
+    if (!qrCanvas) {
 
         alert("Generate QR first");
 
@@ -61,16 +75,13 @@ downloadBtn.addEventListener("click", () => {
     const link = document.createElement("a");
 
 
-    link.href = qrImage.src;
-
     link.download = "BD-QR-Code.png";
 
 
-    document.body.appendChild(link);
+    link.href = qrCanvas.toDataURL("image/png");
+
 
     link.click();
-
-    document.body.removeChild(link);
 
 
 });
